fa1bc5196978621ee3c471b9a2aae63fb3b246b1
Killian THORON <killian.thoron@niji.fr>
2015-09-06 15:00:03 +0200

    Use POEditor API to download content of file...

---
+++ POEditor/lib/utils/log.rb
@@ -0,0 +1,15 @@
+module Log
+
+  def self.title(str) # bg yellow
+    puts "\e[44;37m#{str}\e[0m"
+  end
+
+  def self.error(str)
+    puts "\e[1;31m! #{str}\e[0m"
+  end
+
+  def self.info(str)
+    puts "\e[1;32m√ #{str}\e[0m"
+  end
+
+end
