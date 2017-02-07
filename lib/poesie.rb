e3c6c6b8ee2bc8fe82ed8d43a28ab26a161f95d6
Olivier Halligon <olivier.halligon@niji.fr>
2017-02-07 16:31:21 +0100

    Removed unused file

--- lib/poesie.rb
+++ lib/poesie.rb
@@ -1,5 +1,4 @@
 require File.expand_path('utils/log', File.dirname(__FILE__))
-require File.expand_path('utils/string', File.dirname(__FILE__))
 
 require File.expand_path('exporter', File.dirname(__FILE__))
 require File.expand_path('android_formatter', File.dirname(__FILE__))
